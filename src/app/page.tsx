c9b436f7ceb0cfaaa54fd4ef410b6d2bb9b1bc16
Phil Shergold <phil.s@broadoakgroup.com>
2025-07-05 00:09:52 +0000

    can you put the weekly schedule overview into the schedule tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,8 +35,9 @@ export default function Home() {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Header />
-      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        {isPrivilegedUser ? <ShiftScheduleOverview /> : <Dashboard />}
+      <main className="flex flex-1 flex-col gap-8 p-4 md:p-8">
+        <Dashboard />
+        {isPrivilegedUser && <ShiftScheduleOverview />}
       </main>
     </div>
   );
